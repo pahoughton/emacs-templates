dd340792bdfb98b926a4cfbf0051a659a3d21c55
paul <>
2012-02-06 20:33:08 +0000

    Generated.

--- emacs-templates-1/template-insert-config.el
+++
@@ -1,4 +0,0 @@
-;; This file is generated DO NOT EDIT
-(setq template-insert-package-name "emacs-templates")
-(setq template-insert-tmpl-dir "/Tools/lib/xemacs/xemacs-packages/etc/emacs-templates")
-(provide 'template-insert-config)
