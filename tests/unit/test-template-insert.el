f165069dc0fa55d9ab3649865d9a7688b79c867c
Paul Houghton <paul4hough@gmail.com>
2018-11-11 16:29:21 -0700

    rework to use ert

--- tests/unit/test-template-insert.el
+++ tests/unit/test-template-insert.el
@@ -1,28 +1,23 @@
-(load-file "../../test_tmp/emacs-test-simple/test-simple.el")
-(setq debug-on-error t)
-(setq load-path (append '("../..") load-path))
-(test-simple-start "test for template-insert.el")
-(assert-t (load-file "../../template-insert.el")
-	  "require template-insert.el failed")
-;; have to add topdir to template search path
-(add-to-list 'template-dir-list "../..")
+;; 2013-10-11 (cc) <paul4hough@gmail.com>
 
-(dolist (tmplfn (directory-files "../.." nil ".tmpl\$" ))
-  ; skip fragments
-  (if (not (string-match "-block.tmpl" tmplfn))
-      (progn
-	
-	(setq fn (replace-regexp-in-string
-		  "default-" "default."
-		  (file-name-sans-extension tmplfn)))
-	(message (concat "  provides template for " fn))
-	(setq tbuf (find-file-noselect fn))
-	(switch-to-buffer tbuf)
-	(assert-t (template-insert)
-		  "templaet-insert function returned nil")
-	(message "%s" (buffer-string))
-	)
+(setq load-path (append '("../..") load-path))
+(ert-deftest test-template-insert ()
+  "validate template-insert function"
+  (should (load-file "../../template-insert.el"))
+  (add-to-list 'template-dir-list "../..")
+  (dolist (tmplfn (directory-files "../.." nil ".tmpl\$" ))
+    ;; skip fragments
+    (if (not (string-match "-block.tmpl" tmplfn))
+	(progn
+	  (setq fn (replace-regexp-in-string
+		    "default-" "default."
+		    (file-name-sans-extension tmplfn)))
+	  ;; (message (concat "  provides template for " fn))
+	  (setq tbuf (find-file-noselect fn))
+	  (switch-to-buffer tbuf)
+	  (should (template-insert))
+	  ;;(message "%s" (buffer-string))
+	  )
+      )
     )
   )
-
-(end-tests)
