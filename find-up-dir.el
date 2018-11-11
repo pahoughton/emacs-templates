a7528d5f51c2ce65216551ec8b62609ed4d1c2b7
Paul Houghton <paul4hough@gmail.com>
2018-11-11 16:29:05 -0700

    bug fix and add test

--- find-up-dir.el
+++ find-up-dir.el
@@ -1,59 +1,19 @@
-;;
-;;  File:	find-up-dir.el
-;;  Project:	mci-emacs-templates
-;;  Desc:
-;;
-;;	Emacs Lisp source
-;;
-;;  Notes:
-;;
-;;  Author(s):   Paul Houghton <paul.houghton@mci.com>
-;;  Created:	 03/03/01 03:35
-;;
-;;  Revision Info: (See ChangeLog or cvs log for revision history)
-;;
-;;	$Author$
-;;	$Date$
-;;	$Name$
-;;	$Revision$
-;;	$State$
-;;
-;;  $Id$
-;;
-
+;; 2001-03-03 (cc) <paul4hough@gmail.com>
+;; Find fn in parent directory
 
 ;;;###autoload
-(defun find-up-dir (fn &optional start-dir)
+(defun find-up-dir (fn &optional from-dir)
   "Search up the path for `fn' and return the path to it
    (i.e. `../..')"
   (interactive)
-  (let ((dir (if start-dir start-dir default-directory))
-	(updir nil)
+  (let ((dir (file-truename (if from-dir from-dir  default-directory)))
+	(found-dir nil)
 	)
-    (setq updir (concat dir "/."))
-    (while (> (length dir) 1)
-      (if (file-exists-p (concat updir "/" fn))
-	  (progn
-	    (setq updir (file-truename updir))
-	    (setq dir "")
-	    )
-	(progn (setq updir (concat updir "/.."))
-	       (let (idx)
-		 (setq idx (or (string-match "/" dir) (length dir))
-		       dir (substring dir (min (1+ idx)
-					       (length dir))))))))
-      updir))
-
+    (setq dir (replace-regexp-in-string "/$" "" dir))
+    (while (and (not found-dir) (> (length dir) 1))
+      (if (file-exists-p (concat dir "/" fn))
+	  (setq found-dir dir)
+	(setq dir (substring (file-name-directory dir) 0 -1))))
+    found-dir))
 
 (provide 'find-up-dir)
-
-;;;
-;;; $Log :$
-;;;
-
-;;; Set XEmacs mode
-;;;
-;;; Local Variables:
-;;; mode: Emacs-lisp
-;;; End:
-
--- tests/unit/test-find-up-dir.el
+++ tests/unit/test-find-up-dir.el
@@ -1,10 +1,18 @@
-(load-file "../../test_tmp/emacs-test-simple/test-simple.el")
-(setq debug-on-error t)
-(setq load-path (append '("../..") load-path))
-(test-simple-start "test for find-up-dir.el")
-(assert-t (load-file "../../find-up-dir.el")
-	  "require template-insert.el failed")
-(assert-equal "/usr"
-	      (find-up-dir "share" "/usr/share/man/man8")
-	      "find-up-dir share not in /usr/share/man/man8")
-(end-tests)
+;; 2018-11-11 (cc) <paul4hough@gmail.com>
+
+(ert-deftest test-find-up-dir ()
+  "validate find-up-dir function"
+  (should (load-file "../../find-up-dir.el"))
+  (let ((pat "found")
+	(fpath (concat default-directory ".testing/dira"))
+	(spath nil))
+    (setq spath (concat fpath "/" pat "/db/dirc"))
+    (make-directory spath 1)
+    (should (equal (find-up-dir pat (concat fpath "/" pat)) fpath))
+    (should (equal (find-up-dir pat spath) fpath))
+    (should (cd fpath))
+    (should (equal (find-up-dir pat) fpath))
+    (should (cd spath))
+    (should (equal (find-up-dir pat) fpath))
+    (should (eql (find-up-dir "not likely to find") nil))
+    ))
